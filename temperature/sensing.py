a94cfca68c62dcd1149c2835b935537f82860b6a
Edwin Lee <leeed2001@gmail.com>
2026-02-21 11:52:39 -0600

    Pulled font into a single file

--- temperature/sensing.py
+++ temperature/sensing.py
@@ -25,7 +25,7 @@ try:  # pragma: no cover
     from urequests import get, put
     # noinspection PyPackageRequirements
     from ujson import load as load_json
-    from st7735 import FONT
+    from font import FONT
     from config import WIFI_NETWORKS, CONNECTED_SENSORS, GITHUB_TOKEN
 except ImportError:
     from temperature.tests.mock import DS18X20  # ds18x20
@@ -35,58 +35,11 @@ except ImportError:
     from temperature.tests.mock import ticks_ms, ticks_diff, localtime  # time
     from temperature.tests.mock import get, put  # urequests
     from temperature.tests.mock import load as load_json  # ujson
-    from temperature.tests.mock import FONT
+    from temperature.font import FONT
     from temperature.config_template import WIFI_NETWORKS, CONNECTED_SENSORS, GITHUB_TOKEN
 
 __version__ = 3
 __revision__ = 7
-
-__diagram__ = """
-  Looking at the Pico from "above", so that you can see the WiFi antenna
-                ┏━━━━┓USB CONNECTION
-          ┌─────┃    ┃─────┐
- GP0  | 01│     ┗━━━━┛     │40 | VBUS
- GP1  | 02│                │39 | VSYS
- GND  | 03│                │38 | GND
- GP2  | 04│     ╭─╮        │37 | 3V3_EN
- GP3  | 05│     │ │        │36 | 3V3(OUT)  - SPLIT TO SCREEN VCC and SENSOR VCC
- GP4  | 06│     ╰─╯        │35 | ADC_VREF
- GP5  | 07│                │34 | GP28  - SENSOR DAT
- GND  | 08│                │33 | GND   - SENSOR GND
- GP6  | 09│    ┌─────┐     │32 | GP27
- GP7  | 10│    │     │     │31 | GP26
- GP8  | 11│    │     │     │30 | RUN
- GP9  | 12│    └─────┘     │29 | GP22  - DEBUG JUMPER 1
- GND  | 13│                │28 | GND   - DEBUG JUMPER 2
- GP10 | 14│                │27 | GP21  - SCREEN LED+
- GP11 | 15│                │26 | GP20  - SCREEN RESET
- GP12 | 16│                │25 | GP19  - SCREEN SDA
- GP13 | 17│                │24 | GP18  - SCREEN SCL
- GND  | 18│                │23 | GND   - SCREEN GND
- GP14 | 19│                │22 | GP17  - SCREEN CS
- GP15 | 20│                │21 | GP16  - SCREEN A0/DC
-          └────────────────┘
-
-  Looking at the TFT screen from below, where you can see the SD card slot
-   ┌─────────────────────────────────────────────────────────────┐
-   │                  ┌─────────────────────────────┐            │ LED-
-   │                  │                             │            │ LED+  - PICO GP21
-   │                  │                             │            │ SD_CS
-   │                  │                             │            │ MOSI
-   │                   ╲                            │            │ MISO
-   │                    │                           │            │ SCK
-   │                    │                           │            │ CS    - PICO GP17
-   │                    └───────────────────────────┘            │ SCL   - PICO GP18
-   │                                                             │ SDA   - PICO GP19
-   │                                                             │ A0    - PICO GP16
-   │                                                             │ RESET - PICO GP20
-   │                                                             │ NC
-   │                                                             │ NC
-   │                                                             │ NC
-   │                                                             │ VCC   - SPLITTER FROM PICO 3V3
-   │                                                             │ GND   - PICO GND
-   └─────────────────────────────────────────────────────────────┘
-"""
 
 
 class Sensor:
