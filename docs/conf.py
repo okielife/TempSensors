aa5dcd6415d891fa941b9b83b1e84dea43a2a1cc
Edwin Lee <leeed2001@gmail.com>
2026-03-03 12:00:11 -0600

    Update conf for latex build, update release to post instructions and models

--- docs/conf.py
+++ docs/conf.py
@@ -6,12 +6,20 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-from os import environ
+from os import environ, path
+from sys import path as syspath
+import datetime
 
 project = 'Temperature Sensing'
-copyright = '2026, Edwin Lee'
+copyright = f"{datetime.datetime.now().year}, Edwin Lee"
 author = 'Edwin Lee'
-release = '0.1'
+conf_dir = path.abspath(path.dirname(__file__))
+repo_root = path.abspath(path.join(conf_dir, '..'))
+# Add the parent directory to sys.path
+syspath.insert(0, repo_root)
+
+from firmware.sensing import __version__, __revision__
+release = f"{__version__}.{__revision__}"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -29,3 +37,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'python_docs_theme'  # 'alabaster'
 html_static_path = ['_static']
+
+latex_documents = [
+    ('assembly', 'assembly.tex', 'Temperature Sensor Assembly Instructions', 'Edwin Lee', 'manual'),
+]
+latex_elements = {
+    'extraclassoptions': 'openany'
+}
